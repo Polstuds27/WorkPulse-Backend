ccd2478643369fcba7ab590f2d5af2ac3606565d
Polstuds27 <paul.mojal@gmail.com>
2025-12-08 16:49:03 +0800

    s

--- firebase/firebase-config.js
+++ firebase/firebase-config.js
@@ -1,7 +1,7 @@
 import admin from "firebase-admin"
 import "dotenv/config";
 admin.initializeApp({
-  credential: admin.credential.cert(process.env.FIREBASE_ADMIN_KEY),
+  credential: admin.credential.cert(JSON.parse(process.env.FIREBASE_ADMIN_KEY)),
   databaseURL: process.env.DATABASE_URL
 });
 
